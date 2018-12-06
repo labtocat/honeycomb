ee984d36ef5c390e6dd269975796e3fbe29f7251
Sachin Philip Mathew <me@imsach.in>
2018-12-06 11:54:03 +0530

    removed auto discovery and loaded netflix eureka

--- appolo/greeting/src/main/java/com/example/demo/GreetingApplication.java
+++ appolo/greeting/src/main/java/com/example/demo/GreetingApplication.java
@@ -4,13 +4,27 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+// import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.sleuth.Sampler;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableConfigurationProperties(GreetingProperties.class)
+// eureka
+// @EnableDiscoveryClient
+@EnableEurekaClient
 public class GreetingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GreetingApplication.class, args);
+	}
+
+	// sleth settings
+	@Bean
+	public Sampler getSampler() {
+		return new AlwaysSampler();
 	}
 
 }
